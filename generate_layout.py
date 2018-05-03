cce2e8f92c0d9a4c4fee94d638cc9315fbd5e8d4
MarkJGx <38701409+MarkJGx@users.noreply.github.com>
2018-05-03 17:03:31 +0300

    added sheet reading

--- generate_layout.py
+++ generate_layout.py
@@ -7,12 +7,13 @@ from apiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
 
-
+# usage ./generate_layout.py "sheet_id" "range_name"
 parser = argparse.ArgumentParser()
 parser.add_argument('spreadsheet_id', type=str)
+parser.add_argument('range_name', type=str)
+
 
 args = parser.parse_args()
-
 
 SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
 store = file.Storage('credentials.json')
@@ -21,14 +22,16 @@ if not creds or creds.invalid:
     flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
     creds = tools.run_flow(flow, store)
 service = build('sheets', 'v4', http=creds.authorize(Http()))
-RANGE_NAME = 'Class Data!A2:E'
+RANGE_NAME = 'Sheet4!A1:C'
 result = service.spreadsheets().values().get(spreadsheetId=args.spreadsheet_id,
-                                             range=RANGE_NAME).execute()
+                                             range=args.range_name).execute()
+# converts Google Sheet data into a dict
 values = result.get('values', [])
 if not values:
     print('No data found.')
 else:
-    print('Name, Major:')
+    print('Variable Names, Row, Column Range:')
     for row in values:
         # Print columns A and E, which correspond to indices 0 and 4.
-        print('%s, %s' % (row[0], row[4]))
+        # Rows and Columns start at 1 in Sheets
+        print('%s, %s, %s' % (row[0], row[1], row[2]))
